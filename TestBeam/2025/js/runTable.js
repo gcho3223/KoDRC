d831080557a1e79f2c317ee6e2eab7fa5a8b6ec6
gcho <109217041+gcho3223@users.noreply.github.com>
2026-05-07 22:50:23 +0200

    Update runTable.js

--- TestBeam/2025/js/runTable.js
+++ TestBeam/2025/js/runTable.js
@@ -1,1 +1,45 @@
+let allRuns = [];
 
+async function loadRuns() {
+
+  const response = await fetch("data/runs.json");
+
+  allRuns = await response.json();
+
+  showRunCategory("calib");
+}
+
+function showRunCategory(categoryId) {
+
+  const tbody = document.getElementById("run-table-body");
+
+  tbody.innerHTML = "";
+
+  const filteredRuns = allRuns.filter(
+    run => run.category === categoryId
+  );
+
+  filteredRuns.forEach(run => {
+
+    const row = `
+      <tr>
+        <td>${run.runNumber}</td>
+        <td>${run.energy}</td>
+        <td>${run.particle}</td>
+        <td>${run.angle}</td>
+        <td>${run.note}</td>
+      </tr>
+    `;
+
+    tbody.innerHTML += row;
+  });
+}
+
+function toggleHadronMenu() {
+
+  const subMenu = document.getElementById("hadron-sub-menu");
+
+  subMenu.classList.toggle("is-hidden");
+}
+
+loadRuns();
